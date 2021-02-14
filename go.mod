ea6b83409343f2bdf3895927ff0ae20e486d2ac9
vyzo <vyzo@hackzen.org>
2021-02-14 15:23:08 +0200

    relayd daemon

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/vyzo/libp2p-relay
+
+go 1.15
+
+require (
+	github.com/ipfs/go-log v1.0.4
+	github.com/libp2p/go-libp2p v0.13.0
+	github.com/libp2p/go-libp2p-circuit v0.4.1-0.20210212202441-b77591875d0c
+	github.com/libp2p/go-libp2p-connmgr v0.2.4
+	github.com/libp2p/go-libp2p-core v0.8.1-0.20210204112723-43260f46b7ee
+	github.com/libp2p/go-libp2p-noise v0.1.2
+	github.com/libp2p/go-libp2p-quic-transport v0.10.0
+	github.com/libp2p/go-libp2p-tls v0.1.3
+	github.com/multiformats/go-multiaddr v0.3.1
+)
---
+++ relayd/main.go
@@ -0,0 +1,242 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/libp2p/go-libp2p-circuit/v2/relay"
+
+	"github.com/libp2p/go-libp2p"
+	connmgr "github.com/libp2p/go-libp2p-connmgr"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	noise "github.com/libp2p/go-libp2p-noise"
+	quic "github.com/libp2p/go-libp2p-quic-transport"
+	tls "github.com/libp2p/go-libp2p-tls"
+	"github.com/libp2p/go-libp2p/p2p/protocol/identify"
+
+	logging "github.com/ipfs/go-log"
+	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr/net"
+
+	_ "net/http/pprof"
+)
+
+type Config struct {
+	// pprof
+	PprofPort int
+	// Networking
+	QUICOnly      bool
+	ListenAddrs   []string
+	AnnounceAddrs []string
+	// Connection Manager Limits
+	ConnMgrLo    int
+	ConnMgrHi    int
+	ConnMgrGrace time.Duration
+	// Relay Limits
+	RelayLimitDuration time.Duration
+	RelayLimitData     int64
+	// Relay Resources
+	ReservationTTL        time.Duration
+	ReservationRefreshTTL time.Duration
+	MaxReservations       int
+	MaxCircuits           int
+	BufferSize            int
+}
+
+func init() {
+	identify.ClientVersion = "relayd/0.1"
+	logging.SetLogLevel("relay", "DEBUG")
+}
+
+func main() {
+	idPath := flag.String("-id", "identity", "identity key file path")
+	cfgPath := flag.String("-config", "", "json configuration file; empty uses the default configuration")
+	flag.Parse()
+
+	cfg, err := loadConfig(*cfgPath)
+	if err != nil {
+		panic(err)
+	}
+	privk, err := loadIdentity(*idPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var opts []libp2p.Option
+
+	opts = append(opts,
+		libp2p.Identity(privk),
+		libp2p.DisableRelay(),
+		libp2p.Security(noise.ID, noise.New),
+		libp2p.Security(tls.ID, tls.New),
+		libp2p.ListenAddrStrings(cfg.ListenAddrs...),
+	)
+
+	if len(cfg.AnnounceAddrs) > 0 {
+		var addrs []ma.Multiaddr
+		for _, s := range cfg.AnnounceAddrs {
+			a := ma.StringCast(s)
+			addrs = append(addrs, a)
+		}
+		opts = append(opts,
+			libp2p.AddrsFactory(func([]ma.Multiaddr) []ma.Multiaddr {
+				return addrs
+			}),
+		)
+	}
+
+	if cfg.QUICOnly {
+		opts = append(opts,
+			libp2p.Transport(quic.NewTransport),
+		)
+	} else {
+		opts = append(opts,
+			libp2p.DefaultTransports,
+			libp2p.Transport(quic.NewTransport),
+		)
+	}
+
+	cm := connmgr.NewConnManager(
+		cfg.ConnMgrLo,
+		cfg.ConnMgrHi,
+		cfg.ConnMgrGrace,
+	)
+	opts = append(opts,
+		libp2p.ConnectionManager(cm),
+	)
+
+	ctx := context.Background()
+	host, err := libp2p.New(ctx, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("I am %s\n", host.ID())
+	fmt.Printf("Public Addresses:\n")
+	for _, addr := range host.Addrs() {
+		if manet.IsPublicAddr(addr) {
+			fmt.Printf("\t%s/p2p/%s\n", addr, host.ID())
+		}
+	}
+
+	go listenPprof(cfg.PprofPort)
+
+	time.Sleep(10 * time.Millisecond)
+	fmt.Printf("starting relay...\n")
+
+	_, err = relay.New(ctx, host, relay.WithResources(cfg.Resources()))
+	if err != nil {
+		panic(err)
+	}
+
+	select {}
+}
+
+func loadConfig(cfgPath string) (Config, error) {
+	cfg := defaultConfig()
+
+	if cfgPath != "" {
+		cfgFile, err := os.Open(cfgPath)
+		if err != nil {
+			return Config{}, err
+		}
+		defer cfgFile.Close()
+
+		decoder := json.NewDecoder(cfgFile)
+		err = decoder.Decode(&cfg)
+		if err != nil {
+			return Config{}, err
+		}
+	}
+
+	return cfg, nil
+}
+
+func defaultConfig() Config {
+	return Config{
+		PprofPort:             6060,
+		QUICOnly:              true,
+		ListenAddrs:           []string{"/ip4/0.0.0.0/udp/4001/quic"},
+		ConnMgrLo:             1<<17 + 1<<16, // 192K
+		ConnMgrHi:             1 << 18,       // 256K
+		ConnMgrGrace:          5 * time.Minute,
+		RelayLimitDuration:    time.Minute,
+		RelayLimitData:        1 << 16, // 64K
+		ReservationTTL:        time.Hour,
+		ReservationRefreshTTL: 5 * time.Minute,
+		MaxReservations:       1 << 16, // 64K
+		MaxCircuits:           16,
+		BufferSize:            1024,
+	}
+}
+
+func (cfg *Config) Resources() relay.Resources {
+	return relay.Resources{
+		Limit: &relay.RelayLimit{
+			Duration: cfg.RelayLimitDuration,
+			Data:     cfg.RelayLimitData,
+		},
+		ReservationTTL:        cfg.ReservationTTL,
+		ReservationRefreshTTL: cfg.ReservationRefreshTTL,
+		MaxReservations:       cfg.MaxReservations,
+		MaxCircuits:           cfg.MaxCircuits,
+		BufferSize:            cfg.BufferSize,
+	}
+}
+
+func loadIdentity(idPath string) (crypto.PrivKey, error) {
+	if _, err := os.Stat(idPath); err == nil {
+		return readIdentity(idPath)
+	} else if os.IsNotExist(err) {
+		fmt.Printf("Generating peer identity in %s", idPath)
+		return generateIdentity(idPath)
+	} else {
+		return nil, err
+	}
+}
+
+func readIdentity(path string) (crypto.PrivKey, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return crypto.UnmarshalPrivateKey(bytes)
+}
+
+func generateIdentity(path string) (crypto.PrivKey, error) {
+	privk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	bytes, err := crypto.MarshalPrivateKey(privk)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ioutil.WriteFile(path, bytes, 0400)
+
+	return privk, err
+}
+
+func listenPprof(p int) {
+	addr := fmt.Sprintf("localhost:%d", p)
+	fmt.Printf("registering pprof debug http handler at: http://%s/debug/pprof/\n", addr)
+	switch err := http.ListenAndServe(addr, nil); err {
+	case nil:
+		// all good, server is running and exited normally.
+	case http.ErrServerClosed:
+		// all good, server was shut down.
+	default:
+		// error, try another port
+		fmt.Printf("error registering pprof debug http handler at: %s: %s\n", addr, err)
+		panic(err)
+	}
+}
